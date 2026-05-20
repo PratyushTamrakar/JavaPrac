2e35adc71f22876730c6c81a3f86d1e46f943906
PratyushTamrakar <pratamkfa@gmail.com>
2026-05-20 23:50:43 +0545

    completed the simpleBank program

--- src/SimpleBank.java
+++ src/SimpleBank.java
@@ -1,60 +1,106 @@
 import java.util.Scanner;
 
 public class SimpleBank {
+
+    static double balance = 1000;
+    static Scanner sc = new Scanner(System.in);
+    static int opt;
+    static boolean quit = true;
+
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-        int opt ;
-        boolean quit=true;
-
         while(quit){
+
             System.out.println("*--------------------------------------------------------------------*");
             System.out.println("*--------------------------  Bank  System  --------------------------*");
             System.out.println("Options : ");
-            System.out.println("1)Deposit Money    2)Withdraw Money     3)Check Balance     4)Exit  ");
+            System.out.println("1)Deposit Money    2)Withdraw Money     3)Check Balance     4)Exit");
             System.out.println("*--------------------------------------------------------------------*");
             System.out.println();
+
             System.out.print("Choose the number(1-4) : ");
             opt = sc.nextInt();
 
+            System.out.println("\n*--------------------------------------------------------------------*\n");
+
             switch (opt){
+
                 case 1:
-                    System.out.println("");
+
+                    double addedMoney = depositMoney();
+                    balance += addedMoney;
+
+                    System.out.printf("Balance : Rs.%.2f\n\n", balance);
                     break;
+
                 case 2:
-                    System.out.println("hello2");
+
+                    double withdrawnMoney = withdrawMoney();
+
+                    if(withdrawnMoney <= balance){
+
+                        balance -= withdrawnMoney;
+
+                        System.out.println("Withdrawal Successful.");
+                        System.out.printf("Balance : Rs.%.2f\n\n", balance);
+
+                    } else {
+
+                        System.out.println("Insufficient Balance.\n");
+                    }
+
                     break;
+
                 case 3:
-                    System.out.println("hello3");
+
+                    checkBalance();
                     break;
+
                 case 4:
-                    quit= Exit(quit);
-                    System.out.println("\n*--------------------------------------------------------------------*");
-                    System.out.println();
+
+                    quit = Exit(quit);
+
                     System.out.println("Thank you for using our bank....\n");
                     break;
+
                 default:
+
                     System.out.println("Invalid input. Input number between 1 and 4. !!!!");
             }
+
             System.out.println("*--------------------------------------------------------------------*");
-
         }
+
+        sc.close();
     }
 
-    //Deposit money
-    static double depositMoney(double despositmoney){
-        return despositmoney;
-    };
-    //Withdraw money
-    static double withdrawMoney(double withdrawMoney){
-        return withdrawMoney;
+    // Deposit money
+    static double depositMoney(){
+
+        System.out.printf("Current Balance : Rs.%.2f\n", balance);
+
+        System.out.print("Enter amount to deposit : ");
+        return sc.nextDouble();
     }
-    //Check balance
-    static double checkBalance(double balance){
-        return balance;
+
+    // Withdraw money
+    static double withdrawMoney(){
+
+        System.out.printf("Current Balance : Rs.%.2f\n", balance);
+
+        System.out.print("Enter amount to withdraw : ");
+        return sc.nextDouble();
     }
-    //Exit program
+
+    // Check balance
+    static void checkBalance(){
+
+        System.out.printf("Current Balance : Rs.%.2f\n\n", balance);
+    }
+
+    // Exit program
     static boolean Exit(boolean exit){
+
         return !exit;
     }
 }
