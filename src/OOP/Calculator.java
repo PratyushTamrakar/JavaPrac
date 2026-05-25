7438619437076ab386833c50476d67f24b6ca696
PratyushTamrakar <pratamkfa@gmail.com>
2026-05-25 23:10:25 +0545

    added inteMain

--- src/OOP/Calculator.java
+++ src/OOP/Calculator.java
@@ -2,3 +2,20 @@ package OOP;
 
 public class Calculator {
 }
+/*2. Calculator Class
+
+Create a class Calculator.
+
+Requirements:
+Create methods:
+add(int a, int b)
+subtract(int a, int b)
+multiply(int a, int b)
+divide(int a, int b)
+Each method should print the result directly.
+In main():
+Create a Calculator object
+Call all methods with sample values
+Bonus:
+
+ */
--- src/OOP/StudentInfo.java
+++ src/OOP/StudentInfo.java
@@ -2,3 +2,20 @@ package OOP;
 
 public class StudentInfo {
 }
+/*
+1. Student Info Program
+
+Create a class Student.
+
+Fields:
+name
+age
+marks
+Requirements:
+Create a constructor to set all values.
+Create a method:
+display() → prints all student details
+In main():
+Create 2 student objects
+Call display() for both
+ */
