fb7553f98c060c8a8201e71d54c709415141efa2
PratyushTamrakar <pratamkfa@gmail.com>
2026-05-28 23:52:17 +0545

    updated inheritance animal and dog class

---
+++ src/OOP/Animal.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Animal {
+    boolean isAlive = true;
+    String name ;
+    Animal(boolean isAlive, String name){
+        this.name =name;
+        this.isAlive = isAlive;
+    }
+
+}
---
+++ src/OOP/Dog.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class Dog extends Animal {
+
+    Dog(boolean isAlive, String name) {
+        super(isAlive, name);
+    }
+}
