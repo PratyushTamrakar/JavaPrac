2431682ea8eb5319442c024983495c243996bdd0
PratyushTamrakar <pratamkfa@gmail.com>
2026-05-30 20:49:20 +0545

    created organism parent class

---
+++ src/OOP/Inheritance/Main.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance;
+
+public class Main{
+    public static void main(String[] args){
+        Animal ani = new Animal();
+
+        System.out.println(ani.isAlive);
+    }
+}
---
+++ src/OOP/Inheritance/Organism.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance;
+
+public class Organism{
+    boolean isAlive;
+    Organism(){
+        isAlive=true;
+    }
+}
